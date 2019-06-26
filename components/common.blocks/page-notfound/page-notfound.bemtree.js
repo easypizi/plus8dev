aa568fad40ef741a7471a91c878b12c45b35e5f2
Easypizi <user@n431.local>
2019-06-26 12:33:10 +0300

    add some improvements to dev dependencies

--- components/common.blocks/page-notfound/page-notfound.bemtree.js
+++ components/common.blocks/page-notfound/page-notfound.bemtree.js
@@ -1,41 +1,41 @@
-block('page-notfound').content()(function() {
-    return [
+block("page-notfound").content()(function() {
+  return [
     {
-      block: 'header',
+      block: "header",
       menu: this.data.menu
     },
     {
-      elem: 'content',
+      elem: "content",
       content: [
-      {
-        elem: 'error',
-        content: [
-        '404',
         {
-          elem: 'text',
-          content: 'Упс :('
+          elem: "error",
+          content: [
+            "404",
+            {
+              elem: "text",
+              content: "Упс :("
+            }
+          ]
         },
-        ]
-      },
-      {
-        elem: 'text',
-        content: 'Cтраница не найдена'
-      },
-      {
-        block: 'button',
-        mix: { block: 'page-notfound', elem: 'comeback' },
-        mods: {
-            theme: 'plus',
-            size: 'l',
-            type: 'link'
+        {
+          elem: "text",
+          content: "Cтраница не найдена"
         },
-        url: '/',
-        text: 'Перейти на главную страницу'
-      }
+        {
+          block: "button",
+          mix: { block: "page-notfound", elem: "comeback" },
+          mods: {
+            theme: "plus",
+            size: "l",
+            type: "link"
+          },
+          url: "/",
+          text: "Перейти на главную страницу"
+        }
       ]
     },
     {
-      block: 'footer'
+      block: "footer"
     }
-    ]
+  ];
 });
